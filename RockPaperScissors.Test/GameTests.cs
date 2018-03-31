5212354e2662cfc1c10b6754674d5ead3b05a80a
Jason Gorman <jason.gorman@codemanship.com>
2018-03-31 09:21:32 +0100

    Extracted individual test methods in GameTests

--- RockPaperScissors.Test/GameTests.cs
+++ RockPaperScissors.Test/GameTests.cs
@@ -8,35 +8,17 @@ namespace RockPaperScissors.Test
         {
 // Game tests
             Console.WriteLine("Game tests...");
+            TestPlayer1WinsGame();
+            TestPlayer2WinsGame();
+            TestDrawersNotCounted();
+            TestInvalidMovesNotCounted();
+        }
 
-            // player 1 wins game
-            SpyGameListener listener = new SpyGameListener();
-            Game game = new Game(listener);
-            game.PlayRound(Hand.Rock, Hand.Scissors);
-            game.PlayRound(Hand.Rock, Hand.Scissors);
-
-            int result = listener.Winner;
-            Assert.Equals(1, result, "player 1 wins game");
-
-            // player 2 wins game
-            listener = new SpyGameListener();
-            game = new Game(listener);
-            game.PlayRound(Hand.Rock, Hand.Paper);
-            game.PlayRound(Hand.Rock, Hand.Paper);
-
-            result = listener.Winner;
-            Assert.Equals(2, result, "player 2 wins game");
-
-            // drawers not counted
-            listener = new SpyGameListener();
-            game = new Game(listener);
-            game.PlayRound(Hand.Rock, Hand.Rock);
-            game.PlayRound(Hand.Rock, Hand.Rock);
-
-            result = listener.Winner;
-            Assert.Equals(0, result, "drawers not counted");
-
-            //invalid moves not counted
+        private void TestInvalidMovesNotCounted()
+        {
+            SpyGameListener listener;
+            Game game;
+            int result;
             listener = new SpyGameListener();
             game = new Game(listener);
             try
@@ -50,6 +32,45 @@ namespace RockPaperScissors.Test
 
             result = listener.Winner;
             Assert.Equals(0, result, "invalid moves not counted");
+        }
+
+        private void TestDrawersNotCounted()
+        {
+            SpyGameListener listener;
+            Game game;
+            int result;
+            listener = new SpyGameListener();
+            game = new Game(listener);
+            game.PlayRound(Hand.Rock, Hand.Rock);
+            game.PlayRound(Hand.Rock, Hand.Rock);
+
+            result = listener.Winner;
+            Assert.Equals(0, result, "drawers not counted");
+        }
+
+        private void TestPlayer2WinsGame()
+        {
+            SpyGameListener listener;
+            Game game;
+            int result;
+            listener = new SpyGameListener();
+            game = new Game(listener);
+            game.PlayRound(Hand.Rock, Hand.Paper);
+            game.PlayRound(Hand.Rock, Hand.Paper);
+
+            result = listener.Winner;
+            Assert.Equals(2, result, "player 2 wins game");
+        }
+
+        private void TestPlayer1WinsGame()
+        {
+            SpyGameListener listener = new SpyGameListener();
+            Game game = new Game(listener);
+            game.PlayRound(Hand.Rock, Hand.Scissors);
+            game.PlayRound(Hand.Rock, Hand.Scissors);
+
+            int result = listener.Winner;
+            Assert.Equals(1, result, "player 1 wins game");
         }
     }
 }
