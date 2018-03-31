8a05c9704e7ff1bb965df3e0307f2372ceb86e70
Jason Gorman <jason.gorman@codemanship.com>
2018-03-31 08:21:25 +0100

    Extraced individual test methods in RoundTests.cs

--- RockPaperScissors.Test/RoundTests.cs
+++ RockPaperScissors.Test/RoundTests.cs
@@ -13,42 +13,17 @@ namespace RockPaperScissors.Test
 
         internal void RunAll()
         {
-// Round tests
             Console.WriteLine("Round tests...");
 
-            // rock blunts scissors
-            int result = new Round().Play(Hand.Rock, Hand.Scissors);
-            AssertEquals(result, 1, "rock blunts scissors (Rock, Scissors)");
+            TestRockBluntsScissors();
+            TestScissorsCutPaper();
+            TestPaperWrapsRock();
+            TestRoundIsADraw();
+            TestInvalidInputsNotAllowed();
+        }
 
-            result = new Round().Play(Hand.Scissors, Hand.Rock);
-            AssertEquals(result, 2, "rock blunts scissors (Scissors, Rock)");
-
-
-            // scissors cut paper
-            result = new Round().Play(Hand.Scissors, Hand.Paper);
-            AssertEquals(result, 1, "scissors cut paper (Scissors, Paper)");
-
-            result = new Round().Play(Hand.Paper, Hand.Scissors);
-            AssertEquals(result, 2, "scissors cut paper (Paper, Scissors)");
-
-            // paper wraps rock
-            result = new Round().Play(Hand.Paper, Hand.Rock);
-            AssertEquals(result, 1, "paper wraps rock (Paper, Rock)");
-
-            result = new Round().Play(Hand.Rock, Hand.Paper);
-            AssertEquals(result, 2, "paper wraps rock (Rock, Paper)");
-
-            // round is a draw
-            result = new Round().Play(Hand.Rock, Hand.Rock);
-            AssertEquals(result, 0, "round is a draw (Rock, Rock)");
-
-            result = new Round().Play(Hand.Scissors, Hand.Scissors);
-            AssertEquals(result, 0, "round is a draw (Scissors, Scissors)");
-
-            result = new Round().Play(Hand.Paper, Hand.Paper);
-            AssertEquals(result, 0, "round is a draw (Paper, Paper)");
-
-            // invalid inputs not allowed
+        private void TestInvalidInputsNotAllowed()
+        {
             Exception exception = null;
 
             try
@@ -70,6 +45,45 @@ namespace RockPaperScissors.Test
                 _testSuite.AddTestFailed();
                 Console.WriteLine("invalid inputs not allowed: FAIL - expected InvalidMoveException");
             }
+        }
+
+        private void TestRoundIsADraw()
+        {
+            int result = new Round().Play(Hand.Rock, Hand.Rock);
+            AssertEquals(result, 0, "round is a draw (Rock, Rock)");
+
+            result = new Round().Play(Hand.Scissors, Hand.Scissors);
+            AssertEquals(result, 0, "round is a draw (Scissors, Scissors)");
+
+            result = new Round().Play(Hand.Paper, Hand.Paper);
+            AssertEquals(result, 0, "round is a draw (Paper, Paper)");
+        }
+
+        private void TestPaperWrapsRock()
+        {
+            int result = new Round().Play(Hand.Paper, Hand.Rock);
+            AssertEquals(result, 1, "paper wraps rock (Paper, Rock)");
+
+            result = new Round().Play(Hand.Rock, Hand.Paper);
+            AssertEquals(result, 2, "paper wraps rock (Rock, Paper)");
+        }
+
+        private void TestScissorsCutPaper()
+        {
+            int result = new Round().Play(Hand.Scissors, Hand.Paper);
+            AssertEquals(result, 1, "scissors cut paper (Scissors, Paper)");
+
+            result = new Round().Play(Hand.Paper, Hand.Scissors);
+            AssertEquals(result, 2, "scissors cut paper (Paper, Scissors)");
+        }
+
+        private void TestRockBluntsScissors()
+        {
+            int result = new Round().Play(Hand.Rock, Hand.Scissors);
+            AssertEquals(result, 1, "rock blunts scissors (Rock, Scissors)");
+
+            result = new Round().Play(Hand.Scissors, Hand.Rock);
+            AssertEquals(result, 2, "rock blunts scissors (Scissors, Rock)");
         }
 
         private void AssertEquals(int result, int expected, string displayName)
